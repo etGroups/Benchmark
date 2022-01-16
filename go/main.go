e0b7e7772eefcf9b30a12c866568ab2d8a873d88
MushiX CSys <yxmusashixy@gmail.com>
2022-01-16 18:16:59 +0100

    add hello to go

--- go/main.go
+++ go/main.go
@@ -10,17 +10,26 @@ func main() {
     app := fiber.New()
 
     app.Get("/", func (c *fiber.Ctx) error {
-        return c.SendString("Hello, World!")
+        return c.SendString("Hello, World! [GO]")
     })
 
-    app.Get("/ws", websocket.New(func(c *websocket.Conn) {
-      // Websocket logic
+    app.Get("/hello", websocket.New(func(c *websocket.Conn) {
+        msg := []byte("Hello World")
+        c.WriteMessage(1, msg);
+    }))
+
+    app.Get("/db", websocket.New(func(c *websocket.Conn) {
+
+    }))
+
+    app.Get("/*", websocket.New(func(c *websocket.Conn) {
       for {
         mtype, msg, err := c.ReadMessage()
         if err != nil {
           break
         }
         log.Printf("Read: %s", msg)
+        log.Printf("mtype: %s", mtype)
 
         err = c.WriteMessage(mtype, msg)
         if err != nil {
