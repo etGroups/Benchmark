70a70dd85ca3f0a76b48514980c780f047f0ba95
Emil <emil@dworniczak.pl>
2022-03-08 18:32:43 +0100

    add nodeFastify

---
+++ nodeFastify/index.js
@@ -0,0 +1,47 @@
+import fastifyFramework from "fastify";
+import fastify_websocket from "fastify-websocket";
+import CustomerResource from "./resources/customers.js";
+
+const fastify = fastifyFramework()
+fastify.register(fastify_websocket)
+
+fastify.get('/*', {websocket: true}, (con /* SocketStream */, req /* FastifyRequest */) => {
+	con.socket.on('message', async message => {
+		switch (request) {
+			case "HelloWS":
+				con.socket.send("Hello World");
+				break;
+			case "SqlWS":
+				const customerResource = new CustomerResource();
+				const customers = await customerResource.getCustomers();
+				con.socket.send(JSON.stringify(customers));
+				break;
+			default:
+				con.socket.send(message.toString());
+		}
+	})
+})
+
+fastify.get('/HelloHTTP', async (req, res) => {
+	return 'Hello World'
+})
+
+fastify.post('/PongHTTP', async (req, res) => {
+	return req.body;
+})
+
+fastify.get('/SqlHTTP', async (req, res) => {
+	const customerResource = new CustomerResource();
+	const customers = await customerResource.getCustomers();
+	return JSON.stringify(customers);
+})
+
+const start = async () => {
+	try {
+		await fastify.listen(80, '0.0.0.0');
+	} catch (err) {
+		fastify.log.error(err)
+		process.exit(1)
+	}
+}
+start();
